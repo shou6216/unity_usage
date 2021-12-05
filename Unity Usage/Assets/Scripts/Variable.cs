e10cf94e23ca54f46dc5b5450bca40d4859aa53f
s-yoshino <shou-y6216@yoshinoda.com>
2021-12-05 21:19:35 +0900

    add update function in c# scripts

--- Unity Usage/Assets/Scripts/Variable.cs
+++ Unity Usage/Assets/Scripts/Variable.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Variable : MonoBehaviour
 {
     int variable = 30;
+    const string CONST_VARIABLE = "XXXXX";
+    string[] arrays = { "aaa", "bbb", "ccc" };
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"Ç±ÇÒÇ…ÇøÇÕ{variable}");
         Debug.Log(variable);
         Debug.Log(Hello("ç≤ì°"));
+
+        foreach(string a in arrays)
+        {
+            Debug.Log(a);
+        }
     }
 
     // Update is called once per frame
