c2a512e746cd71c695f249612335d8923591c23d
s-yoshino <shou-y6216@yoshinoda.com>
2021-12-05 23:18:27 +0900

    add rigidbody

---
+++ Unity Usage/Assets/Scripts/AddForce.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        rigidbody.AddForce(1000, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Unity Usage/Assets/Scripts/UpdateFunction.cs
+++ Unity Usage/Assets/Scripts/UpdateFunction.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class UpdateFunction : MonoBehaviour
 {
+    public GameObject cube;
+
+    public Light lightComponent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // オブジェクト情報
+        Debug.Log(gameObject.name);
+        // カメラコンポーネント取得
+        Camera camera = gameObject.GetComponent<Camera>();
+        // 任意のオブジェクト
+        Debug.Log(cube.name);
+        // 任意のコンポーネント
+        Debug.Log(lightComponent.color);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Update中");
+        // キー情報取得
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(vertical);
     }
 }
